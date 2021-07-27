9623cea56fea307949e6596863bc6bd621e3699e
Ryan Brower <ryan.brower28@gmail.com>
2021-07-27 13:31:51 -0700

    created page for individual posts

--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,5 @@ def starting_page(request):
 def posts(request):
     return render(request, "blog/all-posts.html")
 
-def post(request):
-    pass
+def post(request, slug):
+    return render(request, "blog/post-detail.html")
